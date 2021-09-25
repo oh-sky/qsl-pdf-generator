e3e3b98b3333e751af23f2430b5c9b6398d8ce08
Yoshihiro Ohsuka <yoshihiro.ohsuka@gmail.com>
2021-09-26 02:51:07 +0900

    output QSL by inputting ADIF log file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML, CSS
+from AdifLogParser import AdifLogParser
 
 
 OUTPUT_HTML_FILE = '/work/output/qsl-cards.html'
@@ -16,47 +17,14 @@ def main():
 
 def parseQsoLog():
     print('Parsing log ...', file=sys.stderr)
-    return {
-        'qsos': [
-            {
-                'callsign': 'JJ1AYZ/1',
-                'transferCallsign': 'JJ1AYZ',
-                'date': '12th/Aug/2021',
-                'time': '11:05',
-                'rst': '51',
-                'band': '430MHz',
-                'mode': 'FM',
-                'rig': 'FT-818ND',
-                'power': '5W',
-                'antenna': '3 ele Yagi',
-                'groundHeight': '1mh',
-                'qth': '東京都北区 飛鳥山公園 #100117',
-                'remarks': 'FBな1st QSO、ありがとうございました。',
-            },
-            {
-                'callsign': '123456',
-                'transferCallsign': '123456',
-                'date': '13th/Sep/2021',
-                'time': '14:59',
-                'rst': '599',
-                'band': '7MHz',
-                'mode': 'A1A',
-                'rig': 'FT-450D',
-                'power': '50W',
-                'antenna': 'DP',
-                'groundHeight': '10mh',
-                'qth': '常置場所',
-                'remarks': 'いつもありがとうございます。',
-            }
-        ]
-    }
+    return AdifLogParser.parse(filename='/work/input/qso-log.adif')
 
 
 def writeOutHtml(qsoLog):
     print('Generating HTML ...', file=sys.stderr)
     env = Environment(loader=FileSystemLoader('./templates'))
     template = env.get_template('index.html')
-    html = template.render(qsoLog)
+    html = template.render({ 'qsos': qsoLog })
 
     fp = open(OUTPUT_HTML_FILE, 'w')
     fp.write(html)
