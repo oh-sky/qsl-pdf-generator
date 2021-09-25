4785e0c749ab1717717716790e7612fab654162c
Yoshihiro Ohsuka <yoshihiro.ohsuka@gmail.com>
2021-09-26 07:45:47 +0900

    fix code according to coding rules

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""main routine of qsl-pdf-generator"""
 import sys
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML, CSS
@@ -10,27 +11,31 @@ CSS_FILE = '/work/styles/style.css'
 
 
 def main():
-    qsoLog = parseQsoLog()
-    writeOutHtml(qsoLog=qsoLog)
-    writeOutPDF()
+    """ main routine """
+    qso_log = parse_qso_log()
+    write_out_html(qso_log=qso_log)
+    write_out_pdf()
 
 
-def parseQsoLog():
+def parse_qso_log():
+    """ parse qso log from log file """
     print('Parsing log ...', file=sys.stderr)
     return AdifLogParser.parse(filename='/work/input/qso-log.adif')
 
 
-def writeOutHtml(qsoLog):
+def write_out_html(qso_log):
+    """ write out html file based on QSO log """
     print('Generating HTML ...', file=sys.stderr)
     env = Environment(loader=FileSystemLoader('./templates'))
     template = env.get_template('index.html')
-    html = template.render({ 'qsos': qsoLog })
+    html = template.render({ 'qsos': qso_log })
 
-    fp = open(OUTPUT_HTML_FILE, 'w')
-    fp.write(html)
+    file = open(OUTPUT_HTML_FILE, 'w')
+    file.write(html)
 
 
-def writeOutPDF():
+def write_out_pdf():
+    """ write out PDF file by printing HTML and CSS files """
     print('Generating PDF ...', file=sys.stderr)
     HTML(
         filename=OUTPUT_HTML_FILE
