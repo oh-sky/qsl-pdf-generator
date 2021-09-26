da0cabf1000f63147ff51c3f67301be6d0ec6885
Yoshihiro Ohsuka <yoshihiro.ohsuka@gmail.com>
2021-09-26 09:46:43 +0900

    support multiple .adi, .adif files

--- app.py
+++ app.py
@@ -1,46 +1,79 @@
 """main routine of qsl-pdf-generator"""
+import glob
+import os
 import sys
+from typing import NamedTuple
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML, CSS
 from adif_log_parser import AdifLogParser
 
-
-OUTPUT_HTML_FILE = '/work/output/qsl-cards.html'
-OUTPUT_PDF_FILE = '/work/output/qsl-cards.pdf'
+INPUT_DIRECTORY = '/work/input/'
+OUTPUT_DIRECTORY = '/work/output/'
 CSS_FILE = '/work/styles/style.css'
 
 
 def main():
     """ main routine """
-    qso_log = parse_qso_log()
-    write_out_html(qso_log=qso_log)
-    write_out_pdf()
+
+    log_files = get_log_file_list()
+
+    for log_file in log_files:
+        print('starting to process {} ...'.format(
+            log_file.basename), file=sys.stderr)
+        html_file_path = OUTPUT_DIRECTORY + log_file.basename + '.html'
+        pdf_file_path = OUTPUT_DIRECTORY + log_file.basename + '.pdf'
+        qso_log = parse_qso_log(log_file_path=log_file.path)
+        write_out_html(qso_log=qso_log, html_file_path=html_file_path)
+        write_out_pdf(html_file_path=html_file_path,
+                      pdf_file_path=pdf_file_path)
 
 
-def parse_qso_log():
+class File(NamedTuple):
+    basename: str
+    path: str
+
+
+def get_log_file_list() -> tuple:
+    """ get log File list """
+
+    log_file_list = []
+    file_patterns = ('*.adi', '*.adif')
+
+    for file_pattern in file_patterns:
+        filepaths = glob.glob(INPUT_DIRECTORY + file_pattern)
+        for filepath in filepaths:
+            log_file_list.append(File(
+                basename=os.path.basename(filepath),
+                path=filepath
+            ))
+
+    return tuple(log_file_list)
+
+
+def parse_qso_log(log_file_path: str):
     """ parse qso log from log file """
-    print('Parsing log ...', file=sys.stderr)
-    return AdifLogParser.parse(filename='/work/input/qso-log.adif')
+    print('  Parsing log ...', file=sys.stderr)
+    return AdifLogParser.parse(filename=log_file_path)
 
 
-def write_out_html(qso_log):
+def write_out_html(qso_log: list, html_file_path: str):
     """ write out html file based on QSO log """
-    print('Generating HTML ...', file=sys.stderr)
+    print('  Generating HTML ...', file=sys.stderr)
     env = Environment(loader=FileSystemLoader('./templates'))
     template = env.get_template('index.html')
-    html = template.render({ 'qsos': qso_log })
+    html = template.render({'qsos': qso_log})
 
-    with open(OUTPUT_HTML_FILE, 'w', encoding='utf-8') as html_file:
+    with open(html_file_path, 'w', encoding='utf-8') as html_file:
         html_file.write(html)
 
 
-def write_out_pdf():
+def write_out_pdf(html_file_path: str, pdf_file_path: str):
     """ write out PDF file by printing HTML and CSS files """
-    print('Generating PDF ...', file=sys.stderr)
+    print('  Generating PDF ...', file=sys.stderr)
     HTML(
-        filename=OUTPUT_HTML_FILE
+        filename=html_file_path
     ).write_pdf(
-        target=OUTPUT_PDF_FILE,
+        target=pdf_file_path,
         stylesheets=[CSS(CSS_FILE)]
     )
 
