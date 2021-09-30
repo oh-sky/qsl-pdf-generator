51b1e03bcc31037621a44e728adb94912bff8587
Yoshihiro Ohsuka <yoshihiro.ohsuka@gmail.com>
2021-09-30 14:07:11 +0900

    move .py sources into qso_pdf_publisher/

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="qsl_pdf_publisher",
+    version='0.1.2',
+    description='Publish QSL cards as PDF from QSO log files.',
+    author='Yoshihiro Ohsuka',
+    author_email='yoshihiro.ohsuka@gmail.com',
+    url='https://github.com/oh-sky/qsl_pdf_publisher',
+)
